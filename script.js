a76e850b02aef65821cefc565b151e15ff3c4942
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-01-23 11:28:53 +0100

    i

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// button.addEventListener("mousemove", (e) => {
+// button.addEventListener("mousemove", (e) => {button
 //   const rect = button.getBoundingclientRect();
 //   const x = e.clientX - rect.left;
 //   const y = e.clientY - rect.top;
